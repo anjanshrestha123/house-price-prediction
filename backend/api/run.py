d440bfbd7f6f1daf17822944e70f7b4e4ee83c9c
Anjan Shrestha <anjan.shrestha@gfs.com>
2023-03-03 16:29:30 -0500

    fixed host

--- backend/api/run.py
+++ backend/api/run.py
@@ -2,4 +2,4 @@ from config.config import app
 from config.config import config
 
 if __name__ == '__main__':
-    app.run(port=config.get('App', 'server.port'))
+    app.run(host='0.0.0.0', port=config.get('App', 'server.port'))
